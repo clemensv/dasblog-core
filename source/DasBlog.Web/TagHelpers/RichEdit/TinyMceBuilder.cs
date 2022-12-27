aba121403333deb04358b76dc18c8faa3263ac71
Clemens Vasters <clemens@vasters.com>
2022-12-27 19:40:40 +0100

    textpattern support for tinymce

--- source/DasBlog.Web/TagHelpers/RichEdit/TinyMceBuilder.cs
+++ source/DasBlog.Web/TagHelpers/RichEdit/TinyMceBuilder.cs
@@ -38,7 +38,7 @@ namespace DasBlog.Web.TagHelpers.RichEdit
 					<script>
 					tinymce.init({{
 						selector: '#{0}',
-						plugins: 'code',
+						plugins: 'code textpattern',
 						relative_urls : false,
 						remove_script_host : true,
 						document_base_url : '" + urlResolver.GetBaseUrl() + @"'
